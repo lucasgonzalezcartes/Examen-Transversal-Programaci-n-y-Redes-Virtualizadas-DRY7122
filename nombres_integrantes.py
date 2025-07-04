a090f8081f4279299b4cca9afbc9121bd5d3af34
NetAcad DEVASC <changeme@example.com>
2025-07-04 06:19:06 +0000

    Primer commit con lista de nombres

---
+++ nombres_integrantes.py
@@ -0,0 +1,12 @@
+# nombres_integrantes.py
+
+# Lista de integrantes del grupo
+integrantes = [
+    "Lucas Gonzalez",
+    "Benjamin Chavez"
+]
+
+# Imprimir la lista en pantalla
+print("Integrantes del grupo:")
+for nombre in integrantes:
+    print(f"- {nombre}")
